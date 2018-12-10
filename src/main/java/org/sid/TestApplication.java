c6465bb0a798b3906e72764ccc387f0ba43c26b0
yassineattaki <yassineattaki@gmail.com>
2018-12-10 19:31:41 +0000

    change

--- src/main/java/org/sid/TestApplication.java
+++ src/main/java/org/sid/TestApplication.java
@@ -14,6 +14,7 @@ public class TestApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("waaakhdem yaaak");
+		System.out.println("waaahia");
 		
 	}
 }
